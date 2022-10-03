caa3d67b81a9405a2bf40d5f974dfda9310ec415
william bowden <williambowden4@gmail.com>
2022-10-02 21:05:18 -0700

    trying to import py into hy and hy into py.

---
+++ greetings.hy
@@ -0,0 +1,3 @@
+(setv this-will-have-underscores "See?")
+(defn greet [name] (print "Hello from Hy," name))
+
---
+++ greetings.py
@@ -0,0 +1,9 @@
+# def greet-pure-python(name):
+#     print("hello,", name)
+
+import hy
+import greetings
+greetings.greet("Foo") # prints "Hello from Hy, Foo"
+print(greetings.this_will_have_underscores) # prints "See?"
+
+
